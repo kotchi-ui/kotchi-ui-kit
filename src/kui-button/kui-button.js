a772989541098b60fc43ac8b453d67296534e1d7
Ahmad Rida Igbariya <ahmad.rida.igbariya@gmail.com>
2020-05-19 02:40:23 +0300

    jsdocs for kui-button

--- src/kui-button/kui-button.js
+++ src/kui-button/kui-button.js
@@ -35,22 +35,44 @@ const Size = {
 };
 
 /**
- * Button element
+ * Button component that has predefined styles and sizes to serve different semantic purposes with few extras.
+ *
+ * @element kui-button
+ *
+ * @slot - The default slot act as the button's inner html
+ *
  */
 @kuiCustomElement({ tagName })
 class KUIButton extends HTMLElement {
+  /**
+   * Predefined styles that serves different semantic purposes
+   * @type {"kui-button-primary"|"kui-button-secondary"|"kui-button-info"|"kui-button-danger"|"kui-button-warning"|"kui-button-success"|"kui-button-link"}
+   * @attr kui-style
+   */
   @kuiAttributeValidator([typeValidator(Style)])
   @kuiAttribute
   kuiStyle = Style.Secondary;
-
+  /**
+   * Predefined sizes smaller or bigger than the default one
+   * @type {null|"kui-button-small"|"kui-button-large"|"kui-button-block"}
+   * @attr kui-size
+   */
   @kuiAttributeValidator([typeValidator(Size)])
   @kuiAttribute
   kuiSize;
-
+  /**
+   * Display the button in an outline style
+   * @type {Boolean}
+   * @attr kui-outline
+   */
   @kuiAttributeValidator([typeValidator(Types.Boolean)])
   @kuiAttribute({ setter: booleanSetter, getter: booleanGetter })
   kuiOutline = false;
-
+  /**
+   * Sets the button in a disabled state
+   * @type {Boolean}
+   * @attr kui-disabled
+   */
   @kuiAttributeValidator([typeValidator(Types.Boolean)])
   @kuiAttribute({ setter: booleanSetter, getter: booleanGetter })
   kuiDisabled = false;
@@ -59,6 +81,9 @@ class KUIButton extends HTMLElement {
     super();
     const { template, selectors } = templateGenerator.call(this, tagName);
     useShadowDom({ host: this, template });
+    /**
+     * The component's DOM elements
+     */
     this.elements = {
       button: this.shadowRoot.querySelector(selectors.button),
     };
@@ -107,18 +132,28 @@ class KUIButton extends HTMLElement {
     }
   }
 
+  /**
+   * Add a click handler for the button
+   * @param {function} handler - The click handler to be added
+   */
   addClickHandler(handler) {
     if (_isFunction(handler)) {
       this.elements.button.addEventListener("click", handler);
     }
   }
-
+  /**
+   * Removes the given click handler from the button
+   * @param {function} handler - The click handler to be removed
+   */
   removeClickHandler(handler) {
     if (handler) {
       this.elements.button.removeEventListener("click", handler);
     }
   }
 
+  /**
+   * Required function that return the elemnt's tag name
+   */
   getTagName() {
     return tagName;
   }
